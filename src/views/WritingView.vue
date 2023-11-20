b0cee3ae10b6f0050bb0f68a5697e2c4e8aa091f
malt9830 <malt9830@stud.kea.dk>
2023-11-20 20:13:29 +0100

    Implemented letter retrieval in Writing/Sorting

--- src/views/WritingView.vue
+++ src/views/WritingView.vue
@@ -1,10 +1,16 @@
 <template>
   <div>
-    <h1>Writing {{ route.params.id }}</h1>
+    <h1 class="text-5xl text-center uppercase">Writing {{ letter.value }}</h1>
   </div>
 </template>
 
 <script setup lang="ts">
+import type { Letter, LetterPath } from '@/interfaces/Home';
+import { useLetterStore } from '@/stores/letterStore';
+
+const letterStore = useLetterStore()
 const router = useRouter()
 const route = useRoute()
+
+const letter: Letter = letterStore.getLetterFromPath(route.params.id as LetterPath)
 </script>
