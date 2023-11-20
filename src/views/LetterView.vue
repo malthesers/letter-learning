0c5f97003a1445d8b474d9756c0b190f932fc8df
malt9830 <malt9830@stud.kea.dk>
2023-11-20 19:45:50 +0100

    Fixed RouterLinks for LetterView

--- src/views/LetterView.vue
+++ src/views/LetterView.vue
@@ -2,8 +2,8 @@
   <div>
     <h1 class="text-5xl text-center uppercase">{{ route.params.id }}</h1>
     <nav class="text-2xl flex justify-center gap-2">
-      <RouterLink to='writing'>Writing</RouterLink>
-      <RouterLink to='sorting'>Sorting</RouterLink>
+      <RouterLink :to="route.params.id + '/writing'">Writing</RouterLink>
+      <RouterLink :to="route.params.id + '/sorting'">Sorting</RouterLink>
     </nav>
   </div>
 </template>
