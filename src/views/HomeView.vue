2d4073203b07110538bf83eda02d1d7955c4b877
malt9830 <malt9830@stud.kea.dk>
2023-11-20 18:02:08 +0100

    Installed auto-imports

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,9 @@
-<script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
-
 <template>
   <main>
-    <TheWelcome />
+    <p>{{ text }}</p>
   </main>
 </template>
+
+<script setup lang="ts">
+const text = ref('ypyppy')
+</script>
