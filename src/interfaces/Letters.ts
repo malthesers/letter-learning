6c46d6a82e1b0646c0ded740f24f3039bbbb46c5
malt9830 <malt9830@stud.kea.dk>
2023-11-23 21:14:44 +0100

    Added types and arrays for consonants and vowels

--- src/interfaces/Letters.ts
+++ src/interfaces/Letters.ts
@@ -1,6 +1,9 @@
 export type LetterValue = 'a' | 'b' | 'c' | 'd' | 'e' | 'f' | 'g' | 'h' | 'i' | 'j' | 'k' | 'l' | 'm' | 'n' | 'o' | 'p' | 'q' | 'r' | 's' | 't' | 'u' | 'v' | 'w' | 'x' | 'y' | 'z' | 'æ' | 'ø' | 'å'
 export type LetterPath = 'a' | 'b' | 'c' | 'd' | 'e' | 'f' | 'g' | 'h' | 'i' | 'j' | 'k' | 'l' | 'm' | 'n' | 'o' | 'p' | 'q' | 'r' | 's' | 't' | 'u' | 'v' | 'w' | 'x' | 'y' | 'z' | 'ae' | 'oe' | 'aa'
 
+export type Consonant = 'b' | 'c' | 'd' | 'f' | 'g' | 'h' | 'j' | 'k' | 'l' | 'm' | 'n' | 'p' | 'q' | 'r' | 's' | 't' | 'v' | 'w' | 'x' | 'z'
+export type Vowel = 'a' | 'e' | 'i' | 'o' | 'u' | 'y' | 'æ' | 'ø' | 'å'
+
 export interface Letter {
   value: LetterValue,
   path: LetterPath
