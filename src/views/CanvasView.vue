fc4076c7f1bf6a31940a78625a1e4bb20868d4c5
malt9830 <malt9830@stud.kea.dk>
2023-11-23 22:06:48 +0100

    Clicking now adds a dot regardless of continuation

--- src/views/CanvasView.vue
+++ src/views/CanvasView.vue
@@ -7,17 +7,17 @@
           <span class="lowercase">{{ letterStore.current?.value }}</span>
         </p>
         <button @click="clearDrawing"
-          class="w-20 h-20 text-5xl duration-200 hover:-rotate-12 active:scale-90">🗑️</button>
+          class="w-16 h-16 text-5xl duration-200 hover:-rotate-12 active:scale-90">🗑️</button>
         <label v-for="colour in strokeColours" :key="colour" :for="colour" :style="{ backgroundColor: colour }"
-          class="w-20 h-20 border-8 border-white grid place-content-center rounded-full cursor-pointer">
+          class="w-16 h-16 border-8 border-white grid place-content-center rounded-full cursor-pointer">
           <input v-model="strokeColour" type="radio" :id="colour" :value="colour" class="w-0 h-0 opacity-0 peer">
-          <div class="w-8 bg-white aspect-square rounded-full duration-200 transform scale-0 peer-checked:scale-100">
+          <div class="w-6 bg-white aspect-square rounded-full duration-200 transform scale-0 peer-checked:scale-100">
           </div>
         </label>
       </div>
       <div class="w-full h-full grid place-content-center overflow-hidden">
         <canvas ref="canvas" width="2600" height="1400" class="bg-white place-self-center" @mousedown="startDrawing"
-          @mousemove="keepDrawing" @mouseup="stopDrawing"></canvas>
+          @mousemove="draw" @mouseup="stopDrawing"></canvas>
       </div>
     </div>
   </main>
@@ -31,6 +31,8 @@ const letterStore = useLetterStore()
 const isDrawing = ref<boolean>(false)
 const canvas = ref<HTMLCanvasElement | null>(null)
 const context = ref<CanvasRenderingContext2D | null>(null)
+const drawingData = ref<any[]>([])
+
 const strokeWidth = ref<number>(10)
 const strokeColour = ref<string>('#000000')
 const strokeColours = ref<string[]>(['#000000', '#4285F6', '#EC4233', '#F9BE04', '#33A955', '#AF52DE', '#FF9501'])
@@ -39,19 +41,21 @@ const strokeColours = ref<string[]>(['#000000', '#4285F6', '#EC4233', '#F9BE04',
 function startDrawing(e: MouseEvent) {
   if (canvas.value && context.value) {
     isDrawing.value = true
-    context.value.beginPath()
     context.value.moveTo(e.clientX - canvas.value.offsetLeft, e.clientY - canvas.value.offsetTop)
     context.value.strokeStyle = strokeColour.value
     context.value.lineWidth = strokeWidth.value
     context.value.lineJoin = 'round'
     context.value.lineCap = 'round'
+    draw(e)
   }
 }
 
-function keepDrawing(e: MouseEvent) {
+function draw(e: MouseEvent) {
   if (canvas.value && context.value && isDrawing.value) {
     context.value.lineTo(e.clientX - canvas.value.offsetLeft, e.clientY - canvas.value.offsetTop)
     context.value.stroke()
+    context.value.beginPath()
+    context.value.moveTo(e.clientX - canvas.value.offsetLeft, e.clientY - canvas.value.offsetTop)
   }
 }
 
