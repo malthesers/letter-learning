16a275fd1e328a4ebb11a5fec74fc51e21781588
malt9830 <malt9830@stud.kea.dk>
2023-11-25 21:09:36 +0100

    Added Danish special character files

---
+++ src/data/oe.ts
@@ -0,0 +1,12 @@
+import type { Letter } from "@/interfaces/Letters";
+
+export const oe:Letter = {
+  value: 'ø',
+  path: 'oe',
+  name: 'Name of Character',
+  images: {
+    character: 'character-image.png',
+    background: 'background-image.png',
+  },
+  sortables: ['ananas', 'mælk', 'småkager', 'and', 'tea', 'cake']
+}
