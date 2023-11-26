31d20898d6fabe698321f27027db41bd8627c065
malt9830 <malt9830@stud.kea.dk>
2023-11-27 00:08:58 +0100

    Re-scaled LetterView character/buttons

--- src/views/LetterView.vue
+++ src/views/LetterView.vue
@@ -2,7 +2,7 @@
   <main :style="{ backgroundImage: `url(/src/assets/background/${letterStore.current?.images.background}.svg)` }"
     class="h-full px-20 bg-cover bg-center grid grid-cols-2">
     <img :src="`/src/assets/character/${letterStore.current?.images.character}.svg`" :alt="letterStore.current?.name"
-      class=" w-80 mx-auto mt-auto">
+      class="max-w-[30rem] h-auto mx-auto mt-auto">
     <nav class="text-2xl flex flex-col justify-center text-black text-center gap-2">
       <RouterLink :to="route.params.id + '/writing'">Writing</RouterLink>
       <RouterLink :to="route.params.id + '/sorting'">Sorting</RouterLink>
