c673c3c418260204d6b0d90cc9b347e26b8123a4
malt9830 <malt9830@stud.kea.dk>
2023-11-27 05:08:19 +0100

    Fixed header hiding during page transitions

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,12 @@
 <template>
   <TheHeader />
-  <RouterView v-slot="{ Component }">
+  <RouterView v-slot="{ Component, route }">
     <Transition name="page" mode="out-in">
-      <component :is="Component" />
+      <component :is="Component" :key="route.path" />
     </Transition>
   </RouterView>
 </template>
 
 <script setup lang="ts">
+const route = useRoute()
 </script>
--- src/components/TheHeader.vue
+++ src/components/TheHeader.vue
@@ -1,5 +1,5 @@
 <template>
-  <header class="fixed w-full bg-zinc-800 bg-opacity-50 text-3xl p-4 flex justify-between">
+  <header class="fixed z-10 w-full bg-zinc-800 bg-opacity-50 text-3xl p-4 flex justify-between">
     <RouterLink to="/">Home</RouterLink>
     <RouterLink v-if="letterStore.current" :to="`/${letterStore.current.path}`" class="uppercase">{{
       letterStore.current.value }}
