539d7b63d59e93e03ff705b162c4c4a41e2647e0
malt9830 <malt9830@stud.kea.dk>
2023-11-28 19:12:25 +0100

    Added character and text to WritingCanvas

--- src/views/WritingView.vue
+++ src/views/WritingView.vue
@@ -1,7 +1,12 @@
 <template>
   <main :style="{ backgroundImage: `url(/src/assets/background/${letterStore.current?.images.background}.svg)` }"
-    class="bg-cover bg-center">
-    <h1 class="text-5xl text-center uppercase">Writing {{ letterStore.current?.value }}</h1>
+    class="h-full px-20 bg-cover bg-center grid grid-cols-[1fr_2fr] gap-4">
+    <img :src="`/src/assets/character/${letterStore.current?.images.character}.svg`" :alt="letterStore.current?.name"
+      class="max-w-[30rem] h-auto mx-auto mt-auto">
+    <div class="w-full h-full mx-auto p-4 bg-slate-700 bg-opacity-70 rounded-3xl">
+      <h1 class="text-5xl text-center">Hjælp {{ letterStore.current?.name }} med at skrive {{ letterStore.current?.value
+      }}!</h1>
+    </div>
   </main>
 </template>
 
