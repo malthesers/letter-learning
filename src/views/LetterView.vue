0cfb0536b9d80b87c22ebe4b2ea51be64b139fa9
malt9830 <malt9830@stud.kea.dk>
2023-11-28 08:46:47 +0100

    Updated LetterView text

--- src/views/LetterView.vue
+++ src/views/LetterView.vue
@@ -3,12 +3,14 @@
     class="h-full bg-cover bg-center grid grid-cols-2">
     <img :src="`/src/assets/character/${letterStore.current?.images.character}.svg`" :alt="letterStore.current?.name"
       class="max-w-[30rem] h-auto mx-auto mt-auto">
-    <nav class="text-2xl flex flex-col justify-center text-black text-center gap-2">
-      <p class="text-4xl text-center mb-2">{{ letterStore.current?.name }}</p>
-      <RouterLink :to="route.params.id + '/writing'">Writing</RouterLink>
-      <RouterLink :to="route.params.id + '/sorting'">Sorting</RouterLink>
-      <RouterLink :to="route.params.id + '/canvas'">Canvas</RouterLink>
-    </nav>
+    <div class="flex flex-col justify-center text-black text-center">
+      <p class="text-4xl text-center mb-4">{{ letterStore.current?.name }}</p>
+      <nav class="w-60 mx-auto text-2xl flex flex-col gap-4">
+        <RouterLink :to="route.params.id + '/writing'">Skrivehuse</RouterLink>
+        <RouterLink :to="route.params.id + '/sorting'">Forbogstav</RouterLink>
+        <RouterLink :to="route.params.id + '/canvas'">Tegnetavle</RouterLink>
+      </nav>
+    </div>
   </main>
 </template>
 
