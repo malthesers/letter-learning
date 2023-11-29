57c15ef995019002cc57ff2b0df6b3be9799032d
malt9830 <malt9830@stud.kea.dk>
2023-11-29 02:34:10 +0100

    Restructured SortingView drag containers

--- src/views/SortingView.vue
+++ src/views/SortingView.vue
@@ -3,8 +3,8 @@
     class="h-full px-20 bg-cover bg-center grid grid-cols-[1fr_2fr] gap-4">
     <img :src="`/src/assets/character/${letterStore.current?.images.character}.svg`" :alt="letterStore.current?.name"
       class="max-w-[30rem] h-auto mx-auto mt-auto">
-    <div class="w-full h-full mx-auto grid grid-cols-2 grid-rows-3 gap-4 p-4">
-      <div class="flex flex-col bg-slate-700 bg-opacity-70 p-4 rounded-3xl col-span-2">
+    <div class="w-full h-full mx-auto flex flex-col gap-4 p-4">
+      <div class="h-60 flex flex-col bg-slate-700 bg-opacity-70 p-4 rounded-3xl col-span-2">
         <p class="text-4xl text-center mb-4">Hvilke ting kan {{ letterStore.current?.name }} lide?</p>
         <draggable v-model="unsortedItems" group="items" item-key="id" :animation="200"
           class="grow flex flex-wrap place-content-center gap-4">
@@ -13,25 +13,27 @@
           </template>
         </draggable>
       </div>
-      <!-- Correct Container -->
-      <div class="flex flex-col bg-green bg-opacity-70 p-4 rounded-3xl row-span-2">
-        <p class="text-7xl text-center mb-4">😄</p>
-        <draggable v-model="goodItems" group="items" item-key="id" :animation="200"
-          class="grow flex flex-wrap place-content-center gap-4">
-          <template #item="{ element }">
-            <img :src="`/src/assets/sortables/${element}.svg`" :alt="element" class="h-24">
-          </template>
-        </draggable>
-      </div>
-      <!-- Incorrect Container -->
-      <div class="flex flex-col bg-red bg-opacity-70 p-4 rounded-3xl row-span-2">
-        <p class="text-7xl text-center mb-4">😟</p>
-        <draggable v-model="badItems" group="items" item-key="id" :animation="200"
-          class="grow flex flex-wrap place-content-center gap-4">
-          <template #item="{ element }">
-            <img :src="`/src/assets/sortables/${element}.svg`" :alt="element" class="h-24">
-          </template>
-        </draggable>
+      <div class="grow grid grid-cols-2 gap-4">
+        <!-- Correct Container -->
+        <div class="flex flex-col bg-green bg-opacity-70 p-4 rounded-3xl">
+          <p class="text-7xl text-center mb-4">😄</p>
+          <draggable v-model="goodItems" group="items" item-key="id" :animation="200"
+            class="grow flex flex-wrap place-content-center gap-4">
+            <template #item="{ element }">
+              <img :src="`/src/assets/sortables/${element}.svg`" :alt="element" class="h-24">
+            </template>
+          </draggable>
+        </div>
+        <!-- Incorrect Container -->
+        <div class="flex flex-col bg-red bg-opacity-70 p-4 rounded-3xl">
+          <p class="text-7xl text-center mb-4">😟</p>
+          <draggable v-model="badItems" group="items" item-key="id" :animation="200"
+            class="grow flex flex-wrap place-content-center gap-4">
+            <template #item="{ element }">
+              <img :src="`/src/assets/sortables/${element}.svg`" :alt="element" class="h-24">
+            </template>
+          </draggable>
+        </div>
       </div>
     </div>
   </main>
