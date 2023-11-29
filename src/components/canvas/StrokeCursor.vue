bfdc54234ae61dab8b57e7971ef44146bde45d7e
malt9830 <malt9830@stud.kea.dk>
2023-11-29 23:30:02 +0100

    Stroke outline becomes black when stroke is white

--- src/components/canvas/StrokeCursor.vue
+++ src/components/canvas/StrokeCursor.vue
@@ -2,8 +2,8 @@
   <div :style="{
     backgroundColor: strokeColour, width: strokeWidth + 'px',
     left: cursorCoords.x + 'px', top: cursorCoords.y + 'px'
-  }"
-    class="absolute z-20 aspect-square outline-2 outline-dashed outline-white rounded-full transform -translate-x-1/2 -translate-y-1/2 pointer-events-none cursor-none">
+  }" :class="[strokeColour === '#000000' ? 'outline-white' : 'outline-black']"
+    class="absolute z-20 aspect-square outline-2 outline-dashed rounded-full transform -translate-x-1/2 -translate-y-1/2 pointer-events-none cursor-none">
   </div>
 </template>
 
