4bb96ff0bf42e749113b21052aa6d0ca0be121ce
malt9830 <malt9830@stud.kea.dk>
2024-02-01 02:16:36 +0100

    Updated store state type declarations

--- src/stores/letterStore.ts
+++ src/stores/letterStore.ts
@@ -5,15 +5,15 @@ import { defineStore } from 'pinia'
 export const useLetterStore = defineStore('letterStore', () => {
   const route = useRoute()
 
-  const consonants = ref<Consonant[]>(['b', 'c', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 'n', 'p', 'q', 'r', 's', 't', 'v', 'w', 'x', 'z'])
-  const vowels = ref<Vowel[]>(['a', 'e', 'i', 'o', 'u', 'y', 'æ', 'ø', 'å'])
-  const letters = ref<Letter[]>(allLetters)
+  const consonants: Ref<Consonant[]> = ref(['b', 'c', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 'n', 'p', 'q', 'r', 's', 't', 'v', 'w', 'x', 'z'])
+  const vowels: Ref<Vowel[]> = ref(['a', 'e', 'i', 'o', 'u', 'y', 'æ', 'ø', 'å'])
+  const letters: Ref<Letter[]> = ref(allLetters)
 
-  const current = computed<Letter | undefined>(() => {
+  const current: ComputedRef<Letter | undefined> = computed(() => {
     return letters.value.find((letter) => letter.path === route.params.id)
   })
 
-  const isVowel = computed<boolean>(() => {
+  const isVowel: ComputedRef<boolean> = computed(() => {
     return vowels.value.includes(current.value?.value as Vowel)
   })
 
